eccc92980f3dc4e8d83f1ee857178769993d29b0
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-12 22:08:43 +0100

    feat: snack7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,20 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// FOR EACH
+let student;
+students.forEach(element => {if(element.id === 2) student = element});
+console.log(student);
+
+console.log('---');
+
+// FILTER
+console.log(students.filter(element => element.id === 2)[0]);
+
+console.log('---');
+
+// FOR
+student = '';
+for(i = 0; i < students.length; i++) if (students[i].id === 2) student = students[i];
+console.log(student);
